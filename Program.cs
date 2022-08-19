5e9aef2450a799d767886eb7ba73eb618ed9de72
Windyx <ygodnik777@gmail.com>
2022-08-19 17:57:13 +0300

    Первая задача

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 1 
+/*int a, b, max, min;
+
+Console.WriteLine("Enter two numbers:");
+
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+if(a>b)
+{
+    max = a;
+    min = b;
+    Console.WriteLine($"max = {max}, min = {min}");
+}
+else if(a<b)
+{
+    max = b;
+    min = a;
+    Console.WriteLine($"max = {max}, min = {min}");
+}
+else Console.WriteLine("Numbers are equals");
+*/
+
+
+
