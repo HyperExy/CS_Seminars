4a5964c414ae3b73fea8f671f90fb96b57eddbce
Windyx <ygodnik777@gmail.com>
2022-08-23 16:50:37 +0300

    2 zadacha 2sem

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача 1 
-int num;
+/*int num;
 int SecondNumber (int number)
 {
     int temp = number % 100;
@@ -11,3 +11,29 @@ Console.WriteLine("Enter the number:");
 num = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine($"2nd number of {num} is {SecondNumber(num)}:");
+*/
+int ThirdNumber(int number)
+{
+    int temp = -1;
+while(number > 99)
+    {
+    if(number <= 999)
+        temp = number % 10;
+    number/=10;
+    }
+
+return temp;
+}
+int num;
+
+Console.WriteLine("Enter the number:");
+
+num = Convert.ToInt32(Console.ReadLine());
+
+int num1 = ThirdNumber(num);
+if(num == -1)
+    Console.WriteLine("No third number");
+else
+    Console.WriteLine($"Third number of {num} is {num1}");
+
+
