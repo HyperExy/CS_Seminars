25a6a37f4c06c0ca6f7d5e457c45f27f5ee310e9
Windyx <ygodnik777@gmail.com>
2022-08-23 14:30:23 +0300

    1 zadacha 2 sem

--- Program.cs
+++ Program.cs
@@ -1,77 +1,13 @@
 ﻿//Задача 1 
-/*int a, b, max, min;
-
-Console.WriteLine("Enter two numbers:");
-
-a = Convert.ToInt32(Console.ReadLine());
-b = Convert.ToInt32(Console.ReadLine());
-
-if(a>b)
+int num;
+int SecondNumber (int number)
 {
-    max = a;
-    min = b;
-    Console.WriteLine($"max = {max}, min = {min}");
+    int temp = number % 100;
+    temp /= 10;
+    return temp;
 }
-else if(a<b)
-{
-    max = b;
-    min = a;
-    Console.WriteLine($"max = {max}, min = {min}");
-}
-else Console.WriteLine("Numbers are equals");
-*/
-
-//Задача 2
-/*int a, b, c, max;
-
-Console.WriteLine("Enter three numbers:");
-
-a = Convert.ToInt32(Console.ReadLine());
-b = Convert.ToInt32(Console.ReadLine());
-c = Convert.ToInt32(Console.ReadLine());
-
-max = a;
-
-if(b>max)
-    if(b>c)
-        max = b;
-    else max = c;
-else 
-    if(c>max)
-        max = c;
-
-Console.WriteLine($"max = {max}");
-*/
-
-//Задача 3
-/*int num;
-
 Console.WriteLine("Enter the number:");
 
 num = Convert.ToInt32(Console.ReadLine());
 
-if(num%2 == 0 && num != 0)
-    Console.WriteLine($"{num} - четное");
-else if(num%2 == 1)
-    Console.WriteLine($"{num} - нечетное");
-    else 
-        Console.WriteLine($"{num} - ноль");
-*/
-
-//Задача 3
-/*int N, current = 1;
-
-Console.WriteLine("Enter the positive number:");
-
-N = Convert.ToInt32(Console.ReadLine());
-
-while(current <= N)
-{
-    if(current%2 == 0)
-        if(current == 2)
-        Console.Write($"{current}");
-        else
-        Console.Write($", {current}");
-    current++;
-}
-*/
+Console.WriteLine($"2nd number of {num} is {SecondNumber(num)}:");
