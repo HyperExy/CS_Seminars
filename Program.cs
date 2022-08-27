36d5af57ecd670f5c53b2473c07a17f00efc5ecc
Windyx <ygodnik777@gmail.com>
2022-08-27 15:41:37 +0300

    1 zadacha 3 sem

--- Program.cs
+++ Program.cs
@@ -1,54 +1,43 @@
-﻿//Задача 1 
-/*int num;
-int SecondNumber (int number)
+﻿void isPolyndrome(int number)
 {
-    int temp = number % 100;
-    temp /= 10;
-    return temp;
-}
-Console.WriteLine("Enter the number:");
+    int output = number;
+    int size = 0;
 
-num = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine($"2nd number of {num} is {SecondNumber(num)}:");
-*/
-//Задача 2
-/*int ThirdNumber(int number)
-{
-    int temp = -1;
-while(number > 99)
+    while(number >= 1)
     {
-    if(number <= 999)
-        temp = number % 10;
-    number/=10;
+        number/=10;
+        size++;
     }
 
-return temp;
-}
-int num;
+    int index = size - 1;
+    int[] num = new int[size];
+    number = output;
+    while(index>=0)
+    {
+        num[index] = number%10;
+        number/=10;
+        index--;
+    }
+    bool pol = false;
 
-Console.WriteLine("Enter the number:");
-
-num = Convert.ToInt32(Console.ReadLine());
-
-int num1 = ThirdNumber(num);
-if(num == -1)
-    Console.WriteLine("No third number");
-else
-    Console.WriteLine($"Third number of {num} is {num1}");
-*/
-//Задача 3
-/*void IsWeekend(int number)
-{
-    if(number == 6 || number == 7)
-    Console.WriteLine("Its a weekend");
+    for(int i = 0, j = size - 1; i <= j; i++, j--)
+            if(num[i]==num[j])
+                    pol = true;
+            else 
+                {
+                    pol = false;
+                    break;
+                }
+    if(pol)
+        Console.WriteLine($"Number {output} is a polyndrome");
     else
-    Console.WriteLine("Its not a weekend");
+        Console.WriteLine($"Number {output} is not a polyndrome");
+
 }
 int num;
 
 Console.WriteLine("Enter the number:");
 
 num = Convert.ToInt32(Console.ReadLine());
-IsWeekend(num);
-*/
+
+isPolyndrome(num);
