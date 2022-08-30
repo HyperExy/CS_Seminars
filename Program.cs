cf7eabe2e25911096058cbcb965597ed8cc6c349
Windyx <ygodnik777@gmail.com>
2022-08-30 10:03:30 +0300

    3 zadachi 4 sem

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ B = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"{A} in degree of {B} is {degree(A, B)}");
 */
 //Задача 2
-int sumNumber(int number)
+/*int sumNumber(int number)
 {
     int output = number;
     int size = 0, sum = 0;
@@ -45,4 +45,38 @@ Console.WriteLine("Enter the number:");
 num = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine($"Sum of numbers in {num} = {sumNumber(num)}");
+*/
 //Задача 3
+/*int[] EnterArr(int n)
+{
+    int[] arr = new int[n];
+    for(int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine($"Enter the [{i+1}] number of array:");
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+    return arr;
+}
+
+void ShowArr(int[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length; i++)
+        {
+            if(i==arr.Length-1)
+                Console.Write($"{arr[i]}");
+            else
+                Console.Write($"{arr[i]}, ");
+        }
+    Console.Write("]");
+}
+
+int size;
+
+Console.WriteLine("Enter the size of array:");
+size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = EnterArr(size);
+
+ShowArr(array);
+*/
