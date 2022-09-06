8303a35c1f3f40fccd4aa3718c0c03716b029b0f
Windyx <ygodnik777@gmail.com>
2022-09-06 10:04:28 +0300

    1 zadacha 6 sem

--- Program.cs
+++ Program.cs
@@ -1,33 +1,25 @@
 ﻿//Задача 1
-/*
-int[] rand(int size)
-{
-    int[] arr = new int[size];
-    for(int i = 0; i< size; i++)
-    {
-        arr[i] = new Random().Next(100, 1000);
-    }
-    return arr;
-
-}
 int count(int[] arr)
 {
     int count = 0;
     for(int i = 0; i < arr.Length; i++)
-        if(arr[i]%2 == 0)
+        if(arr[i] > 0)
         count++;
     return count;
 }
+Console.WriteLine("Enter the size:");
+    int M = Convert.ToInt32(Console.ReadLine());
 
+int[] array = new int[M];
 
-int[] array = rand(10);
-
-Console.WriteLine("Array:");
 for(int i = 0; i < array.Length; i++)
-    Console.Write($"{array[i]} ");
+        {
+        Console.WriteLine($"Enter the [{i}] element");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        }
 Console.WriteLine("");
 Console.WriteLine($"Count = {count(array)}");
-*/
+
 //Задача 2
 /*
 int[] rand(int size)
