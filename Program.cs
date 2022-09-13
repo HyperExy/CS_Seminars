885691e1dd51fb32fbd07531fa53e6a5603d5ab1
Windyx <ygodnik777@gmail.com>
2022-09-13 12:01:14 +0300

    1 zadacha 8 sem

--- Program.cs
+++ Program.cs
@@ -1,19 +1,39 @@
 ﻿//Задача 1
-/*
-double[,] CreateMatrix(int m, int n)
+
+int[,] CreateMatrix(int m, int n)
 {
-    double[,] matrix = new double[m, n];
+    int[,] matrix = new int[m, n];
     for(int i = 0; i < n; i++)
         for(int j = 0; j < m; j++)
-        matrix[i, j] = new Random().Next(10, 100) + new Random().NextDouble() ;
+        matrix[i, j] = new Random().Next(10, 100);
     return matrix;
 }
 
+void Sort(int[,] matrix)
+{   
+    int maxPosition;
+    for(int i = 0; i< matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            maxPosition = j;
+            for(int k = 0; k < matrix.GetLength(1); k++)
+		    {
+			    if(matrix[i,k] < matrix[i, maxPosition]) maxPosition = k;
+			        int temp = matrix[i, j];
+			    matrix[i, j]= matrix[i, maxPosition];
+			    matrix[i, maxPosition] = temp;
+		    }
+        }
+    }
+}
 Console.WriteLine("Enter the size m:");
     int M = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter the size n:");
     int N = Convert.ToInt32(Console.ReadLine());
-double[,] matrix = CreateMatrix(M, N);
+int[,] matrix = CreateMatrix(M, N);
+
+Console.WriteLine("Before sort:");
 
 for(int i = 0; i < N; i++)
     {
@@ -23,7 +43,19 @@ for(int i = 0; i < N; i++)
         }
     Console.WriteLine("");
     }
-*/
+Sort(matrix);
+
+Console.WriteLine("After sort:");
+
+for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < M; j++)
+        {
+        Console.Write($"{matrix[i, j]} ");
+        }
+    Console.WriteLine("");
+    }
+
 //Задача 2
 /*
 int[,] CreateMatrix(int m, int n)
